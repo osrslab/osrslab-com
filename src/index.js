b447866550957e90dfa2e47bec0d22c3b6a90f7e
Thomas <6449361+hsamoht@users.noreply.github.com>
2020-01-29 21:41:49 +0100

    Create header with navbar
    Mobile and desktop friendly header with navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 import { registerReactAxe } from './utils/accessibility/reactAxeUtil';
+import 'index.scss';
 
 ReactDOM.render(
-  <Router>
-    <App/>
-  </Router>,
+  <App />,
   document.getElementById('root'),
 );
 
